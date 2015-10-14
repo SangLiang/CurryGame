c6f4989cfe4678484304ff6738c95b35b1fb6371
Sangliang <378305868@qq.com>
2015-10-14 22:22:00 +0800

    s

---
+++ Assets/Scripts/Basketball_Builder.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Basketball_Builder : MonoBehaviour {
+    public float Temp_Time = 0;
+    public float Build_Time = 2;
+    public GameObject BasketBall;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Temp_Time += Time.deltaTime;
+        if (Temp_Time > Build_Time)
+        {
+            Instantiate(BasketBall, transform.position, transform.rotation);
+          
+            Temp_Time = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/HeroController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.position.x = new Vector3( );
+
+
+        transform.position = new Vector3(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10)).x, transform.position.y,transform.position.z);
+        
+        
+        //将鼠标坐标转换为屏幕坐标然后赋给战机的坐标
+	}
+
+    void OnCollisionEnter2D( Collision2D col) { 
+        if(col.gameObject.tag=="Enemy"){
+            //Destroy(col.gameObject);
+            StartCoroutine(GoNextto());
+
+        }
+    }
+
+
+    IEnumerator GoNextto()
+    {
+        yield return new WaitForSeconds(0.8f);
+        Application.LoadLevel(1);
+    }
+
+}
