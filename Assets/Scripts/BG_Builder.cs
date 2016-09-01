0766d0a248519836d082d3bbfd512295066d9a04
sangliang <378305868@qq.com>
2016-09-01 18:58:09 +0800

    a

--- Assets/Scripts/BG_Builder.cs
+++ Assets/Scripts/BG_Builder.cs
@@ -11,21 +11,15 @@ public class BG_Builder : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-
     }
-
     // Update is called once per frame
     void Update()
     {
         Temp_Time += Time.deltaTime;
        
-
-
         if (Temp_Time >= Build_Time)
         {
-            
             Instantiate(BackGround, transform.position, transform.rotation);
-            
             Temp_Time = 0;
         }
     }
--- Assets/Scripts/Hero/HeroController.cs
+++ Assets/Scripts/Hero/HeroController.cs
@@ -14,16 +14,12 @@ public class HeroController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         //transform.position.x = new Vector3( );
-
-
-        transform.position = new Vector3(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10)).x, transform.position.y,transform.position.z);
-        
+        transform.position = new Vector3(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10)).x, -2,transform.position.z);
         
         //将鼠标坐标转换为屏幕坐标然后赋给战机的坐标
 	}
 
     void OnCollisionEnter2D( Collision2D col) { 
-       
         if(col.gameObject.tag=="Enemy"){
             if(GameObject.Find("LifeController").GetComponent<LifeController>().Life>1){
                 Destroy(col.gameObject);
